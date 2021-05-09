7078639f0779718867136f3cd32b3ab42f72786c
Jim Lind <spoon.vw@gmail.com>
2021-05-09 14:39:48 -0400

    Add downloader and parser to container

--- src/container.ts
+++ src/container.ts
@@ -1,6 +1,8 @@
 import * as awilix from 'awilix';
+import * as axios from 'axios';
 import { Client as DiscordClient } from 'discord.js';
 import { config as configDotenv } from 'dotenv';
+import * as htmlparser2 from 'htmlparser2';
 import { Config } from './models/config';
 
 export class Container {
@@ -15,8 +17,10 @@ export class Container {
         configModel.discordBotToken = process.env.DISCORD_BOT_TOKEN || '';
 
         this.container.register({
+            axios: awilix.asValue(axios),
             config: awilix.asValue(configModel),
             discordClient: awilix.asClass(DiscordClient).classic(),
+            htmlParser2: awilix.asValue(htmlparser2),
         });
 
         // Load all other modules
