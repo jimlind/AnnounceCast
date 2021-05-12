d6a65ccc0bbf5ddec7d378d908cf69cac1f3b313
Jim Lind <spoon.vw@gmail.com>
2021-05-11 22:03:52 -0400

    Update to use the Datastore
    Timeout is fine here.

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { Podcast } from './models/podcast';
 import { DiscordConnection } from './services/discord/discord-connection';
 import { DiscordMessageFactory } from './services/discord/discord-message-factory';
 import { DiscordMessageSender } from './services/discord/discord-message-sender';
+import { PodcastDataStorage } from './services/podcast/podcast-data-storage';
 import { PodcastProcessor } from './services/podcast/podcast-processor';
 
 // Initialize the container
@@ -21,46 +22,81 @@ container
         const serverCount: Number = discordClient.guilds.cache.size;
         console.log(`Discord Client Logged In on ${serverCount} Servers`);
 
+        // Open the database connection
+        const data = container.resolve<PodcastDataStorage>('podcastDataStorage');
+
         // Keeps track of if an active diary entry thread is running
         let threadRunning: boolean = false;
         let interval: NodeJS.Timeout;
-        const processRestInterval: number = 1000; // Give it 1 second to rest
+        const processRestInterval: number = 60000; // Give it up to 60 seconds to rest
 
-        interval = setInterval(() => {
-            if (threadRunning) return;
+        data.setup().then(() => {
+            data.getPostedData().then((postedData: any) => {
+                interval = setInterval(() => {
+                    if (threadRunning) return;
 
-            const feeds = [
-                'https://feeds.simplecast.com/DPfrjtYE', // Hags
-                'https://anchor.fm/s/184b0a38/podcast/rss', // BANZ
-                'https://anchor.fm/s/23694498/podcast/rss', // WR4
-                'https://anchor.fm/s/3a0acd20/podcast/rss', // Nauts
-                'https://anchor.fm/s/12d1fabc/podcast/rss', // 70mm
-                'https://anchor.fm/s/238d77c8/podcast/rss', // Dune
-                'https://anchor.fm/s/3ae14da0/podcast/rss', // Lost
-            ];
-            const processor = container.resolve<PodcastProcessor>('podcastProcessor');
+                    const feeds = [
+                        'https://feeds.simplecast.com/DPfrjtYE', // Film Hags
+                        'https://anchor.fm/s/184b0a38/podcast/rss', // Bat & Spider
+                        'https://anchor.fm/s/23694498/podcast/rss', // Will Run For...
+                        'https://anchor.fm/s/3a0acd20/podcast/rss', // Cinenauts
+                        'https://anchor.fm/s/12d1fabc/podcast/rss', // 70mm
+                        'https://anchor.fm/s/238d77c8/podcast/rss', // Dune Pod
+                        'https://anchor.fm/s/3ae14da0/podcast/rss', // Lost Light
+                    ];
 
-            feeds.forEach((feedUrl: string) => {
-                processor.process(feedUrl).then((podcast: Podcast) => {
-                    const message = container
-                        .resolve<DiscordMessageFactory>('discordMessageFactory')
-                        .build(podcast);
+                    const channels = [
+                        '812883182390607933', // Dev Channel
+                    ];
 
-                    container
-                        .resolve<DiscordMessageSender>('discordMessageSender')
-                        .send('812883182390607933', message)
-                        .then(() => {
-                            console.log('Message Sent');
-                        });
-                });
+                    const processor = container.resolve<PodcastProcessor>('podcastProcessor');
+
+                    let feedCount = 1; // Adjust for length index-zero
+                    feeds.forEach((feedUrl: string, index: number) => {
+                        setTimeout(() => {
+                            processor.process(feedUrl).then((podcast: Podcast) => {
+                                // Prodcast fetching and process completed (the hard part)
+                                // Allow the thread to start again
+                                if (feedCount++ === feeds.length) {
+                                    console.log('processing done.');
+                                    threadRunning = false;
+                                }
+
+                                // Exit early if the podcast isn't latest
+                                if (podcast.episodeGuid === postedData[podcast.showFeed]) {
+                                    console.log('already did it. skipping.');
+                                    return;
+                                }
+
+                                const message = container
+                                    .resolve<DiscordMessageFactory>('discordMessageFactory')
+                                    .build(podcast);
+
+                                channels.forEach((channelId: string) => {
+                                    container
+                                        .resolve<DiscordMessageSender>('discordMessageSender')
+                                        .send(channelId, message)
+                                        .then(() => {
+                                            postedData[podcast.showFeed] = podcast.episodeGuid;
+                                            data.updatePostedData(
+                                                podcast.showFeed,
+                                                podcast.episodeGuid,
+                                            );
+                                        });
+                                });
+                            });
+                        }, 1000 * index);
+                    });
+
+                    threadRunning = true;
+                }, processRestInterval);
             });
-
-            threadRunning = true;
-        }, processRestInterval);
+        });
 
         // Clean up when process is told to end
         onExit((code, signal) => {
             discordClient.destroy();
+            data.close();
             console.log(`Program Terminated ${code}:${signal}`);
         });
     });
