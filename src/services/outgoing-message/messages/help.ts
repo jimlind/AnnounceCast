af8b12ac36857c5cacc7ab47e3705e9344c91ab2
Jim Lind <spoon.vw@gmail.com>
2021-09-29 20:55:59 -0400

    Update Help Message

--- src/services/outgoing-message/messages/help.ts
+++ src/services/outgoing-message/messages/help.ts
@@ -2,7 +2,6 @@ import { RESOLVER } from 'awilix';
 import { MessageEmbed } from 'discord.js';
 import { Config } from '../../../models/config';
 import { DiscordConnection } from '../../discord/discord-connection';
-import { DiscordDataStorage } from '../../discord/discord-data-storage';
 import { PodcastDataStorage } from '../../podcast/podcast-data-storage';
 
 export class Help {
@@ -10,56 +9,49 @@ export class Help {
 
     config: Config;
     discordConnection: DiscordConnection;
-    discordDataStorage: DiscordDataStorage;
     podcastDataStorage: PodcastDataStorage;
     constructor(
         config: Config,
-        discordDataStorage: DiscordDataStorage,
         discordConnection: DiscordConnection,
         podcastDataStorage: PodcastDataStorage,
     ) {
         this.config = config;
-        this.discordDataStorage = discordDataStorage;
         this.discordConnection = discordConnection;
         this.podcastDataStorage = podcastDataStorage;
     }
 
-    build(message: MessageEmbed, guildId: string) {
-        const prefix = this.discordDataStorage.getPrefix(guildId);
+    build(message: MessageEmbed) {
         const feedCount = this.podcastDataStorage.getFeedCount();
         const serverCount = this.discordConnection.getClient().guilds.cache.size;
 
-        message.setTitle(
-            `${this.config.appName} v${this.config.appVersion} (prefix: \`${prefix}\` )`,
-        );
+        message.setTitle(`${this.config.appName} v${this.config.appVersion}`);
         message.setURL('https://github.com/jimlind/discord.podcasts');
-
         message.setDescription(`Tracking ${feedCount} podcasts on ${serverCount} servers.`);
 
-        message.addField('?podcasts', '> View this help message.');
+        message.addField('/help', '> View this help message.');
         message.addField(
-            '?podcasts prefix <value> 🔒 ',
-            "> Set the bot's custom prefix with the string <value> argument.",
+            '/find <keywords>',
+            '> Replies with up to 4 podcasts matching the search keyword(s)',
         );
         message.addField(
-            `${prefix}find <search terms>`,
-            '> Displays up to 4 podcasts matching the <search terms>',
+            '/following',
+            '> Replies with the list of all podcasts (Ids & Names) followed in this channel',
         );
         message.addField(
-            `${prefix}following`,
-            '> Display the podcasts (ids and names) followed in this channel.',
+            '/follow <keywords> 🔒',
+            '> Follow a podcast in this channel matching the search keyword(s)',
         );
         message.addField(
-            `${prefix}follow <url|search tems> 🔒`,
-            '> Follow a podcast in this channel with the feed URL <url> argument or follow the first podcast matching the search terms',
+            '/follow-rss <feed> 🔒',
+            '> Follow a podcast in this channel using an RSS feed',
         );
         message.addField(
-            `${prefix}unfollow <id> 🔒`,
-            '> Unfollow a podcast with the podcast id <id> argument.',
+            '/unfollow <id> 🔒',
+            '> Unfollow a podcast in this channel using the Podcast Id',
         );
         message.addField(
-            `${prefix}play <id>`,
-            '> Play the most recent episode of a podcast with the podcast id <id> argument.',
+            '/play <id>',
+            '> Play the most recent episode of a podcast using the Podcast Id',
         );
         message.setFooter(
             'The 🔒 commands are only available to users with Manage Server permissions.',
