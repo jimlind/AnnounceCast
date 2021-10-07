013580b702dd1dcc58927e8e8ea901fcdb191a36
Jim Lind <spoon.vw@gmail.com>
2021-10-06 23:35:15 -0400

    Create post-dev-latest-episode.ts
    Just a dumb tool that posts the most recent episode to dev. Will help me sort out some formatting stuff.

---
+++ src/tools/post-dev-latest-episode.ts
@@ -0,0 +1,22 @@
+import { Container } from '../container.js';
+import { Bot } from '../services/bot.js';
+import { DiscordConnection } from '../services/discord/discord-connection.js';
+import { PodcastRssProcessor } from '../services/podcast/podcast-rss-processor.js';
+
+const feedUrl = 'https://anchor.fm/s/133d445c/podcast/rss';
+
+const container: Container = new Container('dev');
+container.register().then(() => {
+    container
+        .resolve<PodcastRssProcessor>('podcastRssProcessor')
+        .process(feedUrl, 1)
+        .then((podcast) => {
+            console.log(podcast);
+            return container.resolve<Bot>('bot').sendNewEpisodeAnnouncement(podcast);
+        })
+        .then(() => {
+            // This is a bit silly but registering the container created the client so we need to destroy it
+            container.resolve<DiscordConnection>('discordConnection').getClient().destroy();
+            console.log('✅  Complete');
+        });
+});
