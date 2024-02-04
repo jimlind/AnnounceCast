7d04047d7f4bcbb041906d3b36e8b4e917090113
Jim Lind <spoon.vw@gmail.com>
2024-02-04 10:18:36 -0500

    Update Application Commands

---
+++ src/tools/app-command/put-application-commands.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env ts-node
+/**
+ * Run this command in the terminal for prod or dev commands like this: 
+ * >>> NODE_ENV=production node --loader ts-node/esm src/tools/app-command/put-application-commands.ts 
+ * >>> NODE_ENV=development node --loader ts-node/esm src/tools/app-command/put-application-commands.ts
+ */
+
+import { readFileSync } from 'fs';
+import { Container } from '../../container.js';
+
+try {
+    const container = new Container();
+    run(container);
+} catch (error) {
+    console.log('❌ Unable to set Application Commands');
+    console.log(error);
+}
+
+async function run(container: Container) {
+    await container.register();    
+
+    const discordRest = container.resolve<import('discord.js').REST>('discordRest');
+    const discordCommandRoutes = container.resolve<`/${string}`>('discordCommandRoutes');
+
+    const commandBuffer = readFileSync(new URL('./data/command.json', import.meta.url));
+    const commandData = JSON.parse(commandBuffer.toString());
+
+    const commandList = await discordRest.put(discordCommandRoutes, { body: commandData });
+
+    if (Array.isArray(commandList)) {
+        console.log(`✅  Set ${commandList.length} Application Commands`);
+    } else {
+        console.log('❌ Set Application Commands but incorrect return type');
+    }
+}
