87bc48fb1f1a88f533beb33e4cadbca25a6ae71c
Jim Lind <spoon.vw@gmail.com>
2024-02-04 11:55:19 -0500

    feat: Updated Discord Connection
    Make it more async/await friendly and using updated actions.

--- src/services/discord/discord-connection.ts
+++ src/services/discord/discord-connection.ts
@@ -1,55 +1,52 @@
 import { RESOLVER } from 'awilix';
-import * as discordJS from 'discord.js';
-import { Config } from '../../models/config.js';
 
 export class DiscordConnection {
     static [RESOLVER] = {};
 
+    config: typeof import('../../config.js').default;
+    discordClient: import('discord.js').Client;
+    discordEvents: typeof import('discord.js').Events;
+
     connected: boolean = false;
     locked: boolean = false;
 
-    config: Config;
-    discordClient: discordJS.Client;
-
-    constructor(config: Config, discordClient: discordJS.Client) {
+    constructor(
+        config: typeof import('../../config.js').default,
+        discordClient: import('discord.js').Client,
+        discordEvents: typeof import('discord.js').Events,
+    ) {
         this.config = config;
         this.discordClient = discordClient;
+        this.discordEvents = discordEvents;
     }
 
-    getConnectedClient(): Promise<discordJS.Client> {
-        return new Promise((resolve, reject) => {
-            // If no token set reject the request
-            if (!this.config.discordBotToken) {
-                return reject('No Discord Bot Token Set');
-            }
+    async getClient(): Promise<import('discord.js').Client> {
+        // If no token set reject the request
+        if (!this.config.discord.botToken) {
+            throw new Error('No Discord Bot Token Set');
+        }
 
-            // If the client is connected return it
-            if (this.connected) {
-                return resolve(this.discordClient);
-            }
+        // If the connecting process is already happening reject additional attempts
+        // Multiple connections means something terrible has happened
+        if (this.locked) {
+            throw new Error('Can Not Make Duplicate Connection Attempts');
+        }
 
-            // If the connecting process is happening reject additional attempts
-            // Multiple connections means something terrible has happened
-            if (this.locked) {
-                return reject('Can Not Make Duplicate Connection Attempts');
-            }
+        // If the client is connected return it
+        if (this.connected) {
+            return this.discordClient;
+        }
 
-            // Indicate the connecting process is active
-            this.locked = true;
+        // Indicate the connecting process is active
+        this.locked = true;
 
-            // On ready needs to be setup before login is called or ready events may be missed
-            this.discordClient.on('ready', () => {
-                this.connected = true;
-                this.locked = false;
-
-                return resolve(this.discordClient);
-            });
-            this.discordClient.login(this.config.discordBotToken);
+        this.discordClient.once(this.discordEvents.ClientReady, (readyClient) => {
+            console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+            this.connected = true;
+            this.locked = false;
         });
-    }
 
-    // Will return the discordJS client in use with no gaurantee that it is connected
-    getClient(): discordJS.Client {
+        await this.discordClient.login(this.config.discord.botToken);
         return this.discordClient;
     }
 }
