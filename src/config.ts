0a779ce4beed1a213f498dfe623d5a8a5f733225
Jim Lind <spoon.vw@gmail.com>
2024-02-04 11:54:33 -0500

    feat: New Config Model

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+// It is frowned up on to have multiple .env files but I'm going for it anyway
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+interface Config {
+    app: {
+        name: string,
+        version: string,
+    }
+    discord: {
+        botToken: string,
+        clientId: string,
+    }
+}
+
+const config: Config = {
+    app: {
+        name: process.env.npm_package_name || '',
+        version: process.env.npm_package_version || '',
+    },
+    discord: {
+        botToken: process.env.DISCORD_BOT_TOKEN || '',
+        clientId: process.env.DISCORD_CLIENT_ID || '',
+    }
+}
+
+export default config;
