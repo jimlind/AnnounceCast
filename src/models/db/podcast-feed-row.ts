a24ecb52389682fda0ed35304db9758ec3981348
Jim Lind <spoon.vw@gmail.com>
2024-02-10 19:56:41 -0500

    feat: make the podcast feed row suck less

--- src/models/db/podcast-feed-row.ts
+++ src/models/db/podcast-feed-row.ts
@@ -1,25 +1,11 @@
-export class PodcastFeedRow {
-    private _id: string = '';
-    public get id(): string {
-        return this._id;
-    }
-    public set id(value: string) {
-        this._id = value;
-    }
+interface PodcastFeedRowInterface {
+    id: string;
+    title: string;
+}
 
-    private _url: string = '';
-    public get url(): string {
-        return this._url;
-    }
-    public set url(value: string) {
-        this._url = value;
-    }
-
-    private _title: string = '';
-    public get title(): string {
-        return this._title;
-    }
-    public set title(value: string) {
-        this._title = value;
-    }
+export default class PodcastFeedRow implements PodcastFeedRowInterface {
+    constructor(
+        public id: string,
+        public title: string,
+    ) {}
 }
