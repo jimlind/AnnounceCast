ab2c392e30c4db0e041e477916b07e9e17672ba4
Jim Lind <spoon.vw@gmail.com>
2024-02-13 22:08:38 -0500

    feat: consolidate and finalize new podcast helpers

--- src/services/podcast/podcast-helpers.ts
+++ src/services/podcast/podcast-helpers.ts
@@ -1,21 +1,48 @@
-import { RESOLVER } from 'awilix';
-import { PodcastEpisode } from '../../models/podcast-episode.js';
-import { Podcast } from '../../models/podcast.js';
-import { PodcastDataStorage } from './podcast-data-storage.js';
+import getPodcastFromFeedFunction, { Episode, Podcast } from 'podparse';
+import HttpClient from '../http-client.js';
+import PodcastDataStorage from './podcast-data-storage.js';
 
-export class PodcastHelpers {
-    static [RESOLVER] = {};
+interface PodcastHelpersInterface {
+    readonly getPodcastFromFeed: typeof getPodcastFromFeedFunction;
+    readonly httpClient: HttpClient;
+    readonly podcastDataStorage: PodcastDataStorage;
 
-    podcastDataStorage: PodcastDataStorage;
+    getPodcastFromUrl(url: string): Promise<Podcast>;
+    getMostRecentPodcastEpisode(podcast: Podcast): Episode;
+    mostRecentPodcastEpisodeIsNew(podcast: Podcast): boolean;
+}
 
-    constructor(podcastDataStorage: PodcastDataStorage) {
-        this.podcastDataStorage = podcastDataStorage;
+export default class PodcastHelpers implements PodcastHelpersInterface {
+    constructor(
+        readonly getPodcastFromFeed: typeof getPodcastFromFeedFunction,
+        readonly httpClient: HttpClient,
+        readonly podcastDataStorage: PodcastDataStorage,
+    ) {}
+
+    public async getPodcastFromUrl(feedUrl: string): Promise<Podcast> {
+        const response = await this.httpClient.get(feedUrl, 5000);
+        const podcast = getPodcastFromFeedFunction(response?.data || '');
+        // meta.importFeedUrl is only officially supported in the SoundOn Namespace, but I find it super useful so I'm using it.
+        podcast.meta.importFeedUrl = feedUrl;
+
+        return podcast;
     }
 
-    podcastHasLatestEpisode(podcast: Podcast): boolean {
-        const guidList = this.podcastDataStorage.getPostedFromUrl(podcast.feed);
-        return podcast.episodeList.reduce((accumulator: boolean, current: PodcastEpisode) => {
-            return accumulator || guidList.includes(current.guid);
-        }, false);
+    public getMostRecentPodcastEpisode(podcast: Podcast): Episode {
+        podcast.episodes.sort((a, b) => {
+            return new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime();
+        });
+        const mostRecentEpisode = podcast.episodes.shift();
+        if (!mostRecentEpisode) {
+            throw new Error('Error finding most recent episode.');
+        }
+        return mostRecentEpisode;
+    }
+
+    public mostRecentPodcastEpisodeIsNew(podcast: Podcast): boolean {
+        const mostRecentEpisode = this.getMostRecentPodcastEpisode(podcast);
+        const guidList = this.podcastDataStorage.getPostedFromUrl(podcast.meta.importFeedUrl || '');
+
+        return !guidList.includes(mostRecentEpisode.guid);
     }
 }
