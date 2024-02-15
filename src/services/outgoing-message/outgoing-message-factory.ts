2ba78f1367bc87300cf69172a368840928948deb
Jim Lind <spoon.vw@gmail.com>
2024-02-14 22:45:28 -0500

    feat: patch outgoing message utils

--- src/services/outgoing-message/outgoing-message-factory.ts
+++ src/services/outgoing-message/outgoing-message-factory.ts
@@ -1,70 +1,71 @@
-import { RESOLVER } from 'awilix';
-import { MessageEmbed } from 'discord.js';
-import { PodcastFeedRow } from '../../models/db/podcast-feed-row';
-import { Podcast } from '../../models/podcast';
-import { Followed } from './messages/followed';
-import { Following } from './messages/following';
-import { Help } from './messages/help';
-import { NewEpisode } from './messages/new-episode';
-import { PodcastInfo } from './messages/podcast-info';
+import { EmbedBuilder } from 'discord.js';
+import { Podcast } from 'podparse';
+import PodcastFeedRow from '../../models/db/podcast-feed-row';
+import EpisodeInfo from './messages/episode-info';
+import Followed from './messages/followed';
+import Following from './messages/following';
+import Help from './messages/help';
+import PodcastInfo from './messages/podcast-info';
 
-export class OutgoingMessageFactory {
-    static [RESOLVER] = {}; // So Awilix autoloads the class
-    MESSAGE_COLOR = 0x7ab87a;
+interface OutgoingMessageFactoryInterface {
+    readonly discordEmbedBuilder: typeof EmbedBuilder;
+    readonly episodeInfo: EpisodeInfo;
+    readonly followed: Followed;
+    readonly following: Following;
+    readonly help: Help;
+    readonly podcastInfo: PodcastInfo;
 
-    followed: Followed;
-    following: Following;
-    help: Help;
-    newEpisode: NewEpisode;
-    podcastInfo: PodcastInfo;
+    buildFollowedMessage(podcast: Podcast, rowList: PodcastFeedRow[]): EmbedBuilder;
+    buildUnfollowedMessage(title: string, rowList: PodcastFeedRow[]): EmbedBuilder;
+    buildFollowingMessage(rowList: PodcastFeedRow[]): EmbedBuilder;
+    buildHelpMessage(): Promise<EmbedBuilder>;
+    buildPodcastEpisodeMessage(podcast: Podcast): EmbedBuilder;
+    buildPodcastInfoMessage(podcast: Podcast): Promise<EmbedBuilder>;
+}
+
+export default class OutgoingMessageFactory {
+    readonly messageColor = 0x7ab87a;
 
     constructor(
-        followed: Followed,
-        following: Following,
-        help: Help,
-        newEpisode: NewEpisode,
-        podcastInfo: PodcastInfo,
-    ) {
-        this.followed = followed;
-        this.following = following;
-        this.help = help;
-        this.newEpisode = newEpisode;
-        this.podcastInfo = podcastInfo;
+        readonly discordEmbedBuilder: typeof EmbedBuilder,
+        readonly episodeInfo: EpisodeInfo,
+        readonly followed: Followed,
+        readonly following: Following,
+        readonly help: Help,
+        readonly podcastInfo: PodcastInfo,
+    ) {}
+
+    public buildFollowedMessage(podcast: Podcast, rowList: PodcastFeedRow[]): EmbedBuilder {
+        return this.followed.build(this.buildBaseMessage(), podcast, rowList);
     }
 
-    buildFollowedMessage(podcast: Podcast, rowList: PodcastFeedRow[]): MessageEmbed {
-        const outgoingMessage = this.followed.build(this._buildBaseMessage(), podcast, rowList);
-
-        return outgoingMessage;
-    }
-
-    buildUnfollowedMessage(title: string, rowList: PodcastFeedRow[]): MessageEmbed {
-        const outgoingMessage = this.following.build(this._buildBaseMessage(), rowList);
+    public buildUnfollowedMessage(title: string, rowList: PodcastFeedRow[]): EmbedBuilder {
+        const outgoingMessage = this.following.build(this.buildBaseMessage(), rowList);
         outgoingMessage.setTitle('Unfollow Successful');
-        outgoingMessage.description =
-            `You are no longer following **${title}**\n` + outgoingMessage.description;
+        outgoingMessage.setDescription(
+            `You are no longer following **${title}**\n` + outgoingMessage.data.description,
+        );
 
         return outgoingMessage;
     }
 
-    buildFollowingMessage(rowList: PodcastFeedRow[]): MessageEmbed {
-        return this.following.build(this._buildBaseMessage(), rowList);
+    public buildFollowingMessage(rowList: PodcastFeedRow[]): EmbedBuilder {
+        return this.following.build(this.buildBaseMessage(), rowList);
     }
 
-    buildHelpMessage(): MessageEmbed {
-        return this.help.build(this._buildBaseMessage());
+    public async buildHelpMessage(): Promise<EmbedBuilder> {
+        return await this.help.build(this.buildBaseMessage());
     }
 
-    buildNewEpisodeMessage(podcast: Podcast): MessageEmbed {
-        return this.newEpisode.build(this._buildBaseMessage(), podcast);
+    public buildPodcastEpisodeMessage(podcast: Podcast): EmbedBuilder {
+        return this.episodeInfo.build(this.buildBaseMessage(), podcast);
     }
 
-    buildPodcastInfoMessage(podcast: Podcast): MessageEmbed {
-        return this.podcastInfo.build(this._buildBaseMessage(), podcast);
+    public async buildPodcastInfoMessage(podcast: Podcast): Promise<EmbedBuilder> {
+        return this.podcastInfo.build(this.buildBaseMessage(), podcast);
     }
 
-    _buildBaseMessage(): MessageEmbed {
-        const message = new MessageEmbed().setColor(this.MESSAGE_COLOR);
-        return message;
+    private buildBaseMessage() {
+        return new this.discordEmbedBuilder().setColor(this.messageColor);
     }
 }
--- src/services/outgoing-message/outgoing-message-helpers.ts
+++ src/services/outgoing-message/outgoing-message-helpers.ts
@@ -1,16 +1,21 @@
-import { RESOLVER } from 'awilix';
 import TurndownService from 'turndown';
-import { PodcastFeedRow } from '../../models/db/podcast-feed-row';
+import PodcastFeedRow from '../../models/db/podcast-feed-row';
 
-export class OutgoingMessageHelpers {
-    static [RESOLVER] = {};
+interface OutgoingMessageHelpersInterface {
+    turndownService: TurndownService;
 
+    feedRowsToGridString(rows: PodcastFeedRow[]): string;
+    compressPodcastDescription(podcastDescription: string): string;
+    compressEpisodeDescription(episodeDescription: string): string;
+}
+
+export default class OutgoingMessageHelpers implements OutgoingMessageHelpersInterface {
     turndownService: TurndownService;
     constructor(turndownService: TurndownService) {
         this.turndownService = turndownService;
     }
 
-    feedRowsToGridString(rows: PodcastFeedRow[]): string {
+    public feedRowsToGridString(rows: PodcastFeedRow[]): string {
         const gridRows: Array<string> = ['ID     / TITLE'];
         rows.forEach((row) => {
             gridRows.push(`${row.id} / ${row.title}`);
@@ -18,19 +23,19 @@ export class OutgoingMessageHelpers {
         return gridRows.join('\n');
     }
 
-    compressPodcastDescription(podcastDescription: string): string {
-        const markdown = this._convertTextToMarkdown(podcastDescription);
+    public compressPodcastDescription(podcastDescription: string): string {
+        const markdown = this.convertTextToMarkdown(podcastDescription);
         // Consolidate multiple new lines by replacing duplicate whitespace characters with empty string
         return markdown.replace(/^\s*\n/gm, '');
     }
 
-    compressEpisodeDescription(episodeDescription: string): string {
-        const markdown = this._convertTextToMarkdown(episodeDescription);
+    public compressEpisodeDescription(episodeDescription: string): string {
+        const markdown = this.convertTextToMarkdown(episodeDescription);
         // Match all text up to the next new line
         return markdown.match(/[^\n\r]*/)?.shift() || '';
     }
 
-    _convertTextToMarkdown(input: string): string {
+    private convertTextToMarkdown(input: string): string {
         const stringWithOnlyHtmlBreaks = input.trim().replace(/[\r\n]+/g, '<br>');
         return this.turndownService.turndown(stringWithOnlyHtmlBreaks);
     }
