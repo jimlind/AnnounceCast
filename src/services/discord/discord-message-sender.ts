d87f116964c8a8832d2051ea601ed7925a4c5356
Jim Lind <spoon.vw@gmail.com>
2024-02-16 21:55:56 -0500

    feat: discord message sender

--- src/services/discord/discord-message-sender.ts
+++ src/services/discord/discord-message-sender.ts
@@ -1,53 +1,26 @@
-import { RESOLVER } from 'awilix';
-import { Client, DiscordAPIError, MessageEmbed, TextChannel } from 'discord.js';
-import { DiscordConnection } from './discord-connection.js';
+import { EmbedBuilder, TextChannel } from 'discord.js';
+import DiscordConnection from './discord-connection.js';
 
-export class DiscordMessageSender {
-    static [RESOLVER] = {};
+export default class DiscordMessageSender {
+    constructor(readonly discordConnection: DiscordConnection) {}
 
-    discordConnection: DiscordConnection;
+    public async send(channelId: string, embedBuilder: EmbedBuilder): Promise<Boolean> {
+        const discordClient = await this.discordConnection.getClient();
+        const channel = discordClient.channels.cache.find((ch) => ch.id === channelId);
 
-    constructor(discordConnection: DiscordConnection) {
-        this.discordConnection = discordConnection;
-    }
+        if (!(channel instanceof TextChannel)) {
+            return false;
+        }
+        const botPermissions = channel.permissionsFor(discordClient.user || '');
+        if (!botPermissions?.has(['ViewChannel', 'SendMessages', 'EmbedLinks'])) {
+            return false;
+        }
+        try {
+            await channel.send({ embeds: [embedBuilder] });
+        } catch (error) {
+            return false;
+        }
 
-    send(channelId: string, message: MessageEmbed): Promise<string> {
-        return new Promise((resolve: Function, reject: Function) => {
-            this.discordConnection.getConnectedClient().then((client: Client) => {
-                const channel = client.channels.cache.find((ch) => ch.id === channelId);
-
-                if (!(channel instanceof TextChannel)) {
-                    return reject('No Text Channel Found');
-                }
-
-                const botPermissions = channel.permissionsFor(client.user || '');
-                if (!botPermissions?.has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS'])) {
-                    return reject('Channel Has Bad Permissions');
-                }
-
-                channel
-                    .send({ embeds: [message] })
-                    .then(() => {
-                        return resolve('Successfully Sent Message');
-                    })
-                    .catch((e) => {
-                        if (e instanceof DiscordAPIError) {
-                            return reject(e.message);
-                        }
-                        return reject('Unknown Error');
-                    });
-            });
-        });
-    }
-
-    // This is a barebones message sender.
-    // Not logging success or failures because it shouldn't be used for anything critical
-    sendString(channelId: string, message: string) {
-        this.discordConnection.getConnectedClient().then((client: Client) => {
-            const channel = client.channels.cache.find((ch) => ch.id === channelId);
-            if (channel instanceof TextChannel) {
-                channel.send(message);
-            }
-        });
+        return true;
     }
 }
