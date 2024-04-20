1d04a0bc7f7292770d453a8a115d993b22b4eb85
Jim Lind <spoon.vw@gmail.com>
2024-04-20 13:37:13 -0400

    feat: add testing
    This took way too long to get working.
    Sinon and Mocha for the win.
    I tried other tools and they all sucked for what I needed.

---
+++ test/services/podcast/podcast-helpers.test.ts
@@ -0,0 +1,48 @@
+import { AxiosResponse } from 'axios';
+import { randomUUID } from 'crypto';
+import sinon from 'sinon';
+import HttpClient from '../../../src/services/http-client.js';
+import PodcastDataStorage from '../../../src/services/podcast/podcast-data-storage.js';
+import PodcastHelpers from '../../../src/services/podcast/podcast-helpers.js';
+
+describe('Podcast Helpers Class', function () {
+    describe('getPodcastFromUrl Method', function () {
+        it('should make http client request for feed using the input url', async function () {
+            const mockedGetPodcastFromFeed = sinon.stub().returns({ meta: {} });
+            const mockedHttpClient = sinon.createStubInstance(HttpClient);
+            const mockedPodcastDataStorage = sinon.createStubInstance(PodcastDataStorage);
+
+            const podcastHelpers = new PodcastHelpers(
+                mockedGetPodcastFromFeed,
+                mockedHttpClient,
+                mockedPodcastDataStorage,
+            );
+
+            const feedUrl = randomUUID();
+            await podcastHelpers.getPodcastFromUrl(feedUrl);
+            sinon.assert.calledWith(mockedHttpClient.get, feedUrl, 5000);
+        });
+
+        it('should parse podcast with response from http client request', async function () {
+            const mockedGetPodcastFromFeed = sinon.stub().returns({ meta: {} });
+            const mockedHttpClient = sinon.createStubInstance(HttpClient);
+            const mockedPodcastDataStorage = sinon.createStubInstance(PodcastDataStorage);
+
+            const responseData = randomUUID();
+            const axiosResponse = <AxiosResponse>{ data: responseData };
+            const axiosPromise: Promise<AxiosResponse> = new Promise((resolve) =>
+                resolve(axiosResponse),
+            );
+            (mockedHttpClient.get = sinon.stub()).returns(axiosPromise);
+
+            const podcastHelpers = new PodcastHelpers(
+                mockedGetPodcastFromFeed,
+                mockedHttpClient,
+                mockedPodcastDataStorage,
+            );
+
+            await podcastHelpers.getPodcastFromUrl(randomUUID());
+            sinon.assert.calledWith(mockedGetPodcastFromFeed, responseData);
+        });
+    });
+});
