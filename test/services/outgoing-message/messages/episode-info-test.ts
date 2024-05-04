3b9bd5b874d792c6d388866cb569738c62d64607
Jim Lind <spoon.vw@gmail.com>
2024-05-03 20:14:53 -0400

    feat: more tests

---
+++ test/services/outgoing-message/messages/episode-info-test.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from 'crypto';
+import { EmbedBuilder } from 'discord.js';
+import { Episode, Meta, MetaImage, Podcast } from 'podparse';
+import sinon from 'sinon';
+import EpisodeInfo from '../../../../src/services/outgoing-message/messages/episode-info.js';
+import OutgoingMessageHelpers from '../../../../src/services/outgoing-message/outgoing-message-helpers.js';
+import PodcastHelpers from '../../../../src/services/podcast/podcast-helpers.js';
+
+describe('Episode Info Class', function () {
+    describe('build Method', function () {
+        it('should set the title of the embed', async function () {
+            const title = randomUUID();
+
+            const mockOutgoingMessageHelpers = sinon.createStubInstance(OutgoingMessageHelpers);
+            const mockPodcastHelpers = sinon.createStubInstance(PodcastHelpers);
+            const mockPrettyMilliseconds = sinon.stub();
+            const episodeInfo = new EpisodeInfo(
+                mockOutgoingMessageHelpers,
+                mockPodcastHelpers,
+                mockPrettyMilliseconds,
+            );
+
+            const mockEmbed = sinon.createStubInstance(EmbedBuilder);
+            const image = <MetaImage>{};
+            const meta = <Meta>{ title, image };
+            const episode = <Episode>{ title };
+
+            const podcast = <Podcast>{ meta, episodes: [] };
+
+            mockPodcastHelpers.getMostRecentPodcastEpisode.returns(episode);
+
+            episodeInfo.build(mockEmbed, podcast);
+
+            sinon.assert.calledWith(mockEmbed.setTitle, title);
+        });
+    });
+});
