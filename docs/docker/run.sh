4693e15e98035fc877ecf2f7e443160bec4ed562
Jim Lind <spoon.vw@gmail.com>
2024-05-27 22:50:13 -0400

    fix: remove docker stuff that never really worked

--- docs/docker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-parent_directory="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-docker rm --force podcasts &> /dev/null
-docker build -t podcasts $parent_directory/resources
-docker run --hostname podcasts.local --name podcasts -v $parent_directory/../../:/app -it podcasts /bin/zsh
