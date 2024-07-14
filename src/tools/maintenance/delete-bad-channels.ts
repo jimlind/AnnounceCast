0242cdc503e2fab6c2cfbcd927f7fa3cdc6b3c8a
Jim Lind <spoon.vw@gmail.com>
2024-07-14 08:51:38 -0400

    fix: delete bad channels is better

--- src/tools/maintenance/delete-bad-channels.ts
+++ src/tools/maintenance/delete-bad-channels.ts
@@ -1,18 +1,18 @@
 /**
  * Run this command in the terminal against the local database like this:
- * >>> NODE_ENV=production node --loader ts-node/esm src/tools/maintenance/delete-bad-channels.ts
- * >>> NODE_ENV=development node --loader ts-node/esm src/tools/maintenance/delete-bad-channels.ts
+ * >>> NODE_ENV=production node --import ./register.mjs ./src/tools/maintenance/delete-bad-channels.ts
+ * >>> NODE_ENV=development node --import ./register.mjs ./src/tools/maintenance/delete-bad-channels.ts
  */
 
+import { confirm } from '@inquirer/prompts';
 import bettersqlite3 from 'better-sqlite3';
-import { PermissionsBitField, TextChannel } from 'discord.js';
+import { BaseGuildTextChannel, PermissionsBitField, ThreadChannel } from 'discord.js';
 import fs from 'fs';
 import { Container } from '../../container.js';
 import DiscordConnection from '../../services/discord/discord-connection.js';
 
 const outputFolder = './src/tools/maintenance/output';
 const outputFile = `${outputFolder}/deleted-bad-channels-${Date.now()}.csv`;
-const dryRun = true;
 
 try {
     fs.mkdirSync(outputFolder);
@@ -46,41 +46,66 @@ async function run(container: Container) {
     // Ensure only unique values by using the Set
     channelIdList = Array.from(new Set(channelIdList));
 
+    let badChannelIdList = [];
     for (let index = 0; index < 10; index++) {
         process.stdout.write(`${channelIdList.length} channels to check - `);
-        const badChannelIdList = [];
+        badChannelIdList = [];
 
         for (const channelId of channelIdList) {
             try {
                 const channel = await discordClient.channels.fetch(channelId);
-                if (!(channel instanceof TextChannel)) {
-                    throw new Error('Channel is not a text channel');
+
+                // This covers all the current text channels (announcments, threads, etc)
+                if (
+                    !(channel instanceof BaseGuildTextChannel) &&
+                    !(channel instanceof ThreadChannel)
+                ) {
+                    throw new Error('Channel is not a textual channel');
                 }
 
-                const botPermissions = channel?.permissionsFor(discordClient.user || '');
-                const hasPermissions = botPermissions?.has([
-                    PermissionsBitField.Flags.SendMessages,
-                    PermissionsBitField.Flags.EmbedLinks,
-                ]);
-                if (!hasPermissions) {
-                    throw new Error('Channel does not have permissions');
+                const botPermissions = channel.permissionsFor(discordClient.user || '');
+                // Check that the bot has some ability to send messages
+                if (
+                    !botPermissions?.has(PermissionsBitField.Flags.SendMessages) &&
+                    !botPermissions?.has(PermissionsBitField.Flags.SendMessagesInThreads)
+                ) {
+                    throw new Error('Channel does not have sending permissions');
                 }
+
+                // Check that the bot has can view the channel and embed links
+                if (
+                    !botPermissions?.has([
+                        PermissionsBitField.Flags.ViewChannel,
+                        PermissionsBitField.Flags.EmbedLinks,
+                    ])
+                ) {
+                    throw new Error('Channel does not have view or embed');
+                }
+
                 process.stdout.write('.');
             } catch (error) {
+                badChannelIdList.push({ channelId, error });
+
                 process.stdout.write('✗');
-                badChannelIdList.push(channelId);
             }
         }
         process.stdout.write('\n');
-        channelIdList = badChannelIdList;
+        channelIdList = badChannelIdList.map((x) => x.channelId);
     }
 
-    purgeChannels(channelIdList, database);
+    await purgeChannels(badChannelIdList, database);
     database.close();
 }
 
-async function purgeChannels(channelIdList: string[], database: bettersqlite3.Database) {
-    for (const channelId of channelIdList) {
+async function purgeChannels(
+    channelIdList: { channelId: string; error: unknown }[],
+    database: bettersqlite3.Database,
+) {
+    // Prompt for actual deletes
+    const live = await confirm({ message: 'Perform actual deletes?' });
+
+    for (const channelError of channelIdList) {
+        const channelId = channelError.channelId;
         const query =
             'SELECT url, channel_id FROM feeds f JOIN channels c ON f.id = c.feed_id WHERE channel_id = ?';
         const channelList = database.prepare(query).all(channelId).map(Object);
@@ -89,10 +114,14 @@ async function purgeChannels(channelIdList: string[], database: bettersqlite3.Da
             const url = channel.url || '';
             const cid = channel.channel_id || '';
             fs.appendFileSync(outputFile, `"${url}","${cid}"\n`, 'utf-8');
+
+            const message = `Deleting channel ${cid} with podcast ${url} because "${channelError.error}"\n`;
+            process.stdout.write(message);
         }
 
         let channelsResult;
-        if (!dryRun) {
+        // Do the actual deletes
+        if (live) {
             channelsResult = database
                 .prepare('DELETE FROM channels WHERE channel_id = ?')
                 .run(channelId);
