5f67ce566068d799616aa32755f2763b66921010
Jim Lind <spoon.vw@gmail.com>
2024-07-17 22:46:01 -0400

    feat: add a bunch of logging around a problematic podcast
    Trying to figure out what it is doing so maybe I can replicate it and fix it.
    Every time I load it up manually it works.
    I tried to create some bad feeds (slow, huge, etc) but they all timed out normally.

--- src/services/podcast/podcast-fetch.ts
+++ src/services/podcast/podcast-fetch.ts
@@ -1,26 +1,54 @@
+import { Logger } from 'log4js';
+
 interface PodcastFetchInterface {
+    readonly logger: Logger;
     getPartialPodcastStringFromUrl(input: string, timeout: number): Promise<string>;
 }
 
 export default class PodcastFetch implements PodcastFetchInterface {
+    constructor(readonly logger: Logger) {}
+
     public async getPartialPodcastStringFromUrl(feedUrl: string, timeout: number): Promise<string> {
+        const isProblematicPodcast = feedUrl.includes('meinpodcast');
+
         const controller = new AbortController();
         const timeoutId = setTimeout(() => {
+            if (isProblematicPodcast) {
+                this.logger.debug('ProblematicPodcast Timeout Happened');
+            }
             controller.abort();
         }, timeout);
+
+        if (isProblematicPodcast) {
+            this.logger.debug('ProblematicPodcast Fetch Started');
+        }
 
         const response = await fetch(feedUrl, {
             headers: { 'User-Agent': 'AnnounceCast Cient' },
             signal: controller.signal,
         });
 
+        if (isProblematicPodcast) {
+            this.logger.debug('ProblematicPodcast Fetch Completed', { response });
+        }
+
         // If the response doesn't have an OK success message exit early
         if (response.status !== 200) {
+            if (isProblematicPodcast) {
+                this.logger.debug('ProblematicPodcast Response is not OK', {
+                    status: response.status,
+                });
+            }
             return '';
         }
 
         const streamReader = response.body?.getReader();
         if (!streamReader) {
+            if (isProblematicPodcast) {
+                this.logger.debug('ProblematicPodcast Response can not get reader', {
+                    streamReader,
+                });
+            }
             return '';
         }
 
@@ -28,9 +56,25 @@ export default class PodcastFetch implements PodcastFetchInterface {
         let resultString = '';
         let resultChunk = await streamReader.read();
         while (!resultChunk.done) {
+            if (isProblematicPodcast) {
+                this.logger.debug('ProblematicPodcast has read a chunk');
+            }
+
             resultString += decoder.decode(resultChunk.value);
 
+            if (isProblematicPodcast) {
+                this.logger.debug('ProblematicPodcast has added to result string', {
+                    length: resultString.length,
+                });
+            }
+
             if (this.firstPubDatesAreSequential(resultString)) {
+                if (isProblematicPodcast) {
+                    this.logger.debug('ProblematicPodcast is done checking pubdates', {
+                        length: resultString.length,
+                    });
+                }
+
                 clearTimeout(timeoutId);
 
                 // This feels a little sketchy to abort the controller and return but it works.
@@ -38,7 +82,25 @@ export default class PodcastFetch implements PodcastFetchInterface {
                 return this.cleanPartialPodcastFeed(resultString);
             }
 
+            if (isProblematicPodcast) {
+                this.logger.debug('ProblematicPodcast starting reading another chunk', {
+                    length: resultString.length,
+                });
+            }
+
             resultChunk = await streamReader.read();
+
+            if (isProblematicPodcast) {
+                this.logger.debug('ProblematicPodcast completed reading another chunk', {
+                    length: resultString.length,
+                });
+            }
+        }
+
+        if (isProblematicPodcast) {
+            this.logger.debug('ProblematicPodcast done with the while loop', {
+                length: resultString.length,
+            });
         }
         clearTimeout(timeoutId);
 
