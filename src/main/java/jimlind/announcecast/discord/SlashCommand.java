ec08227b76c8f23cade3e597558cdf1650e1234b
Jim Lind <spoon.vw@gmail.com>
2025-01-18 11:57:02 -0500

    feat: add follow-rss command

--- src/main/java/jimlind/announcecast/discord/SlashCommand.java
+++ src/main/java/jimlind/announcecast/discord/SlashCommand.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import java.util.List;
 import jimlind.announcecast.discord.message.*;
 import jimlind.announcecast.integration.action.FollowAction;
+import jimlind.announcecast.integration.action.FollowRssAction;
 import jimlind.announcecast.integration.action.UnfollowAction;
 import jimlind.announcecast.integration.context.*;
 import jimlind.announcecast.podcast.Podcast;
@@ -13,6 +14,7 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 public class SlashCommand {
   @Inject private FollowAction followAction;
   @Inject private FollowContext followContext;
+  @Inject private FollowRssAction followRssAction;
   @Inject private FollowingContext followingContext;
   @Inject private HelpContext helpContext;
   @Inject private SearchContext searchContext;
@@ -26,6 +28,10 @@ public class SlashCommand {
         switch (event.getName()) {
           case "follow" -> {
             Podcast podcast = this.followAction.run(event);
+            yield FollowMessageList.build(this.followContext.build(event, podcast));
+          }
+          case "follow-rss" -> {
+            Podcast podcast = this.followRssAction.run(event);
             yield FollowMessageList.build(this.followContext.build(event, podcast));
           }
           case "following" -> FollowingMessageList.build(this.followingContext.build(event));
---
+++ src/main/java/jimlind/announcecast/integration/action/FollowRssAction.java
@@ -0,0 +1,27 @@
+package jimlind.announcecast.integration.action;
+
+import com.google.inject.Inject;
+import jimlind.announcecast.podcast.Client;
+import jimlind.announcecast.podcast.ITunes;
+import jimlind.announcecast.podcast.Podcast;
+import jimlind.announcecast.storage.db.Channel;
+import jimlind.announcecast.storage.db.Feed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+
+public class FollowRssAction {
+  @Inject Client client;
+  @Inject Channel channel;
+  @Inject Feed feed;
+  @Inject ITunes iTunes;
+
+  public Podcast run(SlashCommandInteractionEvent event) {
+    OptionMapping feedOption = event.getInteraction().getOption("feed");
+    String feedUrl = feedOption != null ? feedOption.getAsString() : "";
+    Podcast podcast = this.client.createPodcastFromFeedUrl(feedUrl, 0);
+    String feedId = this.feed.addFeed(podcast.getFeedUrl(), podcast.getTitle());
+    this.channel.addChannel(feedId, event.getChannelId());
+
+    return podcast;
+  }
+}
