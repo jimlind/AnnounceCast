137c931148c7b9a9b797363c2bc804d361e492c7
Jim Lind <spoon.vw@gmail.com>
2025-01-18 21:47:01 -0500

    Update BasicModule.java

--- src/main/java/jimlind/announcecast/discord/BasicModule.java
+++ src/main/java/jimlind/announcecast/discord/BasicModule.java
@@ -8,6 +8,7 @@ public class BasicModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(Listeners.class).in(Scopes.SINGLETON);
+    bind(Manager.class).in(Scopes.SINGLETON);
     bind(ShutdownThread.class).in(Scopes.SINGLETON);
     bind(SlashCommand.class).in(Scopes.SINGLETON);
   }
