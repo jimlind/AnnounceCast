431ec08afba5396c0ed604076e481d8553a1417a
Jim Lind <spoon.vw@gmail.com>
2025-01-18 21:01:50 -0500

    refactor: delete unused

--- src/main/java/jimlind/announcecast/discord/BasicModule.java
+++ src/main/java/jimlind/announcecast/discord/BasicModule.java
@@ -8,7 +8,6 @@ public class BasicModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(Listeners.class).in(Scopes.SINGLETON);
-    bind(Message.class).in(Scopes.SINGLETON);
     bind(ShutdownThread.class).in(Scopes.SINGLETON);
     bind(SlashCommand.class).in(Scopes.SINGLETON);
   }
--- src/main/java/jimlind/announcecast/discord/Message.java
+++
@@ -1,9 +0,0 @@
-package jimlind.announcecast.discord;
-
-public class Message {
-  public Message() {}
-
-  public String build() {
-    return "A New Message for a New Day.";
-  }
-}
