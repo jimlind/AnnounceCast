7c550af388f99ffab692bfb9c29694be3f3ff301
Jim Lind <spoon.vw@gmail.com>
2025-01-19 11:58:50 -0500

    Remove contexts from the basicmodule

--- src/main/java/jimlind/announcecast/integration/BasicModule.java
+++ src/main/java/jimlind/announcecast/integration/BasicModule.java
@@ -2,10 +2,7 @@ package jimlind.announcecast.integration;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-import jimlind.announcecast.integration.action.FollowAction;
-import jimlind.announcecast.integration.action.FollowRssAction;
-import jimlind.announcecast.integration.action.UnfollowAction;
-import jimlind.announcecast.integration.context.*;
+import jimlind.announcecast.integration.action.*;
 
 public class BasicModule extends AbstractModule {
 
@@ -13,13 +10,9 @@ public class BasicModule extends AbstractModule {
   protected void configure() {
     // Actions
     bind(FollowAction.class).in(Scopes.SINGLETON);
+    bind(FollowingAction.class).in(Scopes.SINGLETON);
     bind(FollowRssAction.class).in(Scopes.SINGLETON);
+    bind(SearchAction.class).in(Scopes.SINGLETON);
     bind(UnfollowAction.class).in(Scopes.SINGLETON);
-    // Contexts
-    bind(FollowContext.class).in(Scopes.SINGLETON);
-    bind(FollowingContext.class).in(Scopes.SINGLETON);
-    bind(HelpContext.class).in(Scopes.SINGLETON);
-    bind(SearchContext.class).in(Scopes.SINGLETON);
-    bind(UnfollowContext.class).in(Scopes.SINGLETON);
   }
 }
