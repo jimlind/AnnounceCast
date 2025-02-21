072846ee17144d6ff2affad73fc22e6dfa2c129a
Jim Lind <spoon.vw@gmail.com>
2025-02-21 16:39:49 -0500

    feat: actually check if episode is processed

--- src/main/java/jimlind/announcecast/scraper/task/ScrapeSinglePodcast.java
+++ src/main/java/jimlind/announcecast/scraper/task/ScrapeSinglePodcast.java
@@ -5,30 +5,39 @@ import com.google.inject.assistedinject.Assisted;
 import java.util.TimerTask;
 import jimlind.announcecast.podcast.Client;
 import jimlind.announcecast.podcast.Podcast;
+import jimlind.announcecast.scraper.Helper;
 import jimlind.announcecast.scraper.Queue;
+import jimlind.announcecast.storage.db.Joined;
+import jimlind.announcecast.storage.model.PostedFeed;
 
 public class ScrapeSinglePodcast extends TimerTask {
   private final Client client;
+  private final Helper helper;
+  private final Joined joined;
   private final Queue queue;
 
   private final String url;
 
   @Inject
-  public ScrapeSinglePodcast(Client client, Queue queue, @Assisted String url) {
+  public ScrapeSinglePodcast(
+      Client client, Helper helper, Joined joined, Queue queue, @Assisted String url) {
     this.client = client;
+    this.helper = helper;
+    this.joined = joined;
     this.queue = queue;
     this.url = url;
   }
 
   @Override
   public void run() {
-    Podcast podcast = this.client.createPodcastFromFeedUrl(this.url, 1);
-    if (podcast == null) {
+    Podcast podcast = client.createPodcastFromFeedUrl(this.url, 1);
+    PostedFeed postedFeed = joined.getPostedFeedByUrl(this.url);
+    if (podcast == null || postedFeed == null || podcast.getEpisodeList().isEmpty()) {
       return;
     }
 
-    // This is only for example
-    // We actually need to check if there is a new episode
-    this.queue.setPodcast(this.url);
+    if (this.helper.episodeNotProcessed(podcast.getEpisodeList().getFirst(), postedFeed)) {
+      queue.setPodcast(postedFeed.getUrl());
+    }
   }
 }
