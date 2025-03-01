ba74849c5a5573d120ed1eab1d17f34b4544c1b4
Jim Lind <spoon.vw@gmail.com>
2025-03-01 13:14:16 -0500

    fix: you can't create indexes twice
    sqlite doesn't give you a good way to do it either.
    since the effort is entirely optional just let it fail.

--- src/main/java/jimlind/announcecast/storage/db/Connection.java
+++ src/main/java/jimlind/announcecast/storage/db/Connection.java
@@ -18,20 +18,26 @@ public class Connection {
       Statement statement = this.connection.createStatement();
       statement.execute(
           "CREATE TABLE IF NOT EXISTS channels (feed_id TEXT, channel_id TEXT, UNIQUE(feed_id, channel_id))");
-      statement.execute("CREATE INDEX idx_channels_feed_id ON channels(feed_id)");
       statement.execute(
           "CREATE TABLE IF NOT EXISTS feeds (id TEXT PRIMARY KEY, url TEXT UNIQUE, title TEXT)");
-      statement.execute("CREATE INDEX idx_feeds_id ON feeds(id)");
       statement.execute(
           "CREATE TABLE IF NOT EXISTS posted (feed_id TEXT PRIMARY KEY UNIQUE, guid TEXT)");
-      statement.execute("CREATE UNIQUE INDEX idx_posted_feed_id ON posted(feed_id)");
       statement.execute(
           "CREATE TABLE IF NOT EXISTS subscriber (feed_id TEXT PRIMARY KEY UNIQUE, active INTEGER NOT NULL CHECK (active IN (0, 1)))");
-      statement.execute("CREATE UNIQUE INDEX idx_subscriber_feed_id ON subscriber(feed_id)");
-    } catch (SQLException e) {
+    } catch (SQLException ignore) {
       // If there isn't a database connection hard stop
       log.atError().setMessage("Setting Up SQLite failed").log();
       System.exit(-1);
+    }
+
+    try {
+      Statement indexStatement = this.connection.createStatement();
+      indexStatement.execute("CREATE INDEX idx_channels_feed_id ON channels(feed_id)");
+      indexStatement.execute("CREATE INDEX idx_feeds_id ON feeds(id)");
+      indexStatement.execute("CREATE UNIQUE INDEX idx_posted_feed_id ON posted(feed_id)");
+      indexStatement.execute("CREATE UNIQUE INDEX idx_subscriber_feed_id ON subscriber(feed_id)");
+    } catch (SQLException ignore) {
+      // Do nothing if there is an issue creating indexes
     }
   }
 
