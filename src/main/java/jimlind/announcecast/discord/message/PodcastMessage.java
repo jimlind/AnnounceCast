2e1038baa5ba402152bd2332c6e01e87f4c12aa4
Jim Lind <spoon.vw@gmail.com>
2025-03-17 21:57:57 -0400

    fix: empty title or description

--- src/main/java/jimlind/announcecast/discord/message/PodcastMessage.java
+++ src/main/java/jimlind/announcecast/discord/message/PodcastMessage.java
@@ -2,23 +2,30 @@ package jimlind.announcecast.discord.message;
 
 import jimlind.announcecast.Helper;
 import jimlind.announcecast.discord.EmbedBuilder;
+import jimlind.announcecast.podcast.Podcast;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class PodcastMessage {
-  public static MessageEmbed build(jimlind.announcecast.podcast.Podcast podcast) {
-    String feedLine = "Show Feed URL: " + podcast.getFeedUrl();
+  public static MessageEmbed build(Podcast podcast) {
+    String title = null;
+    if (podcast.getTitle() != null && !podcast.getTitle().isBlank()) {
+      title = podcast.getTitle();
+    }
 
+    String feedLine = "Show Feed URL: " + podcast.getFeedUrl();
     String siteLine = "";
     if (podcast.getShowUrl() != null) {
       siteLine = "\nShow's Website: " + podcast.getShowUrl();
     }
 
-    String description = Helper.htmlToMarkdown(podcast.getDescription(), 256);
-    description += "\n\n" + feedLine + siteLine;
+    String description = "";
+    if (podcast.getDescription() != null) {
+      description = Helper.htmlToMarkdown(podcast.getDescription(), 256) + "\n\n";
+    }
+    description += feedLine + siteLine;
 
     EmbedBuilder embedBuilder = new EmbedBuilder();
-
-    embedBuilder.setTitle(podcast.getTitle());
+    embedBuilder.setTitle(title);
     embedBuilder.setDescription(description);
     embedBuilder.setThumbnail(podcast.getImageUrl());
 
