179070938b4a5619a23ac3dc5de64b3060479aee
Jim Lind <spoon.vw@gmail.com>
2025-03-25 22:22:01 -0400

    feat/fix: get members and bug fix
    Ooops.

--- src/main/java/jimlind/announcecast/storage/db/Patreon.java
+++ src/main/java/jimlind/announcecast/storage/db/Patreon.java
@@ -2,17 +2,40 @@ package jimlind.announcecast.storage.db;
 
 import com.google.inject.Inject;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import jimlind.announcecast.patreon.Member;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Patreon {
   private @Inject Connection connection;
 
+  public List<Member> getAllMembers() {
+    List<Member> result = new ArrayList<>();
+    try (Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery("SELECT patreon_id, user_id FROM patreon");
+      while (resultSet.next()) {
+        Member member = new Member();
+        member.setPatreonId(resultSet.getString("patreon_id"));
+        member.setUserId(resultSet.getString("user_id"));
+        member.setFullName("N/A");
+        result.add(member);
+      }
+    } catch (Exception ignore) {
+      log.atWarn().setMessage("Unable to get all members").log();
+    }
+
+    return result;
+  }
+
   public void insertMember(String patreonId, String userId) {
     String insertMember = "INSERT OR IGNORE INTO patreon (patreon_id, user_id) VALUES (?, ?)";
     try (PreparedStatement statement = connection.prepareStatement(insertMember)) {
       statement.setString(1, patreonId);
-      statement.setString(1, userId);
+      statement.setString(2, userId);
       statement.executeUpdate();
     } catch (Exception exception) {
       log.atWarn()
