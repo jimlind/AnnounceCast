03e49ce314b900ec59cf24bc5661987ec81f3a6d
Jim Lind <spoon.vw@gmail.com>
2025-03-25 22:24:36 -0400

    feat: impliment actual record maintenance

--- src/main/java/jimlind/announcecast/patreon/UpdateMembers.java
+++ src/main/java/jimlind/announcecast/patreon/UpdateMembers.java
@@ -1,26 +1,30 @@
 package jimlind.announcecast.patreon;
 
 import com.google.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TimerTask;
+import jimlind.announcecast.storage.db.Patreon;
 import lombok.Setter;
 
 public class UpdateMembers extends TimerTask {
   @Setter private String patreonAccessToken;
   @Inject private Client client;
+  @Inject private Patreon patreon;
 
   @Override
   public void run() {
     List<Member> memberList = this.client.createMemberList(this.patreonAccessToken);
+    List<String> patreonIdList = new ArrayList<>();
     for (Member member : memberList) {
-      System.out.println(member.getFullName());
-      System.out.println(member.getPatreonId());
-      System.out.println(member.getUserId());
+      this.patreon.insertMember(member.getPatreonId(), member.getUserId());
+      patreonIdList.add(member.getPatreonId());
+    }
+
+    for (Member member : this.patreon.getAllMembers()) {
+      if (!patreonIdList.contains(member.getPatreonId())) {
+        this.patreon.deleteMemberByPatreonId(member.getPatreonId());
+      }
     }
   }
-
-  // scrape the patreon api
-  // insert (or reinsert) members into local db
-  // get all members from local db
-  // loop over and delete when missing from the patreon api list
 }
