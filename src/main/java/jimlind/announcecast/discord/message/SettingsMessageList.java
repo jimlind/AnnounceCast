2ae89e5ceff7e3619af80e398b7105214e8768f7
Jim Lind <spoon.vw@gmail.com>
2025-04-15 22:28:45 -0400

    feat: show tags in settings

--- src/main/java/jimlind/announcecast/discord/message/SettingsMessageList.java
+++ src/main/java/jimlind/announcecast/discord/message/SettingsMessageList.java
@@ -1,7 +1,9 @@
 package jimlind.announcecast.discord.message;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import jimlind.announcecast.discord.EmbedBuilder;
+import jimlind.announcecast.integration.PopulatedTag;
 import jimlind.announcecast.integration.context.SettingsContext;
 import jimlind.announcecast.podcast.Podcast;
 import net.dv8tion.jda.api.entities.MessageEmbed;
@@ -12,27 +14,45 @@ public class SettingsMessageList {
       return List.of(AccessDeniedMessage.build());
     }
 
-    return dataMessage(context.getPodcast());
+    return dataMessage(context.getPodcast(), context.getTagList());
   }
 
-  private static List<MessageEmbed> dataMessage(Podcast podcast) {
+  private static List<MessageEmbed> dataMessage(Podcast podcast, List<PopulatedTag> tagList) {
     EmbedBuilder embedBuilder = new EmbedBuilder();
     embedBuilder.setTitle("Settings");
-    String description = "";
+    StringBuilder description = new StringBuilder();
 
     // Prioritized Podcasts
     if (podcast == null) {
-      description = "You have not prioritized any podcasts.\n\n";
+      description.append("You have not prioritized any podcasts.\n\n");
     } else {
-      description =
-          "You have prioritized the %s Podcast from %s.\n\n"
-              .formatted(podcast.getTitle(), podcast.getAuthor());
+      description.append(formatPodcastString(podcast));
     }
 
     // Tagged Podcasts
-    description += "You have not tagged any podcasts.";
-    embedBuilder.setDescription(description);
+    if (tagList.isEmpty()) {
+      description.append("You have not tagged any podcasts.\n\n");
+    } else {
+      description.append("You have tagged:\n");
+      description.append(
+          tagList.stream()
+              .map(SettingsMessageList::formatTagString)
+              .collect(Collectors.joining("\n")));
+    }
 
+    embedBuilder.setDescription(description.toString());
     return List.of(embedBuilder.build());
+  }
+
+  private static String formatPodcastString(Podcast podcast) {
+    String title = podcast.getTitle();
+    String author = podcast.getAuthor();
+
+    return "You have prioritized the %s Podcast from %s.\n\n".formatted(title, author);
+  }
+
+  private static String formatTagString(PopulatedTag tag) {
+    return "\"@%s\" on \"%s\" in \"%s\""
+        .formatted(tag.getRoleName(), tag.getPodcastTitle(), tag.getChannelName());
   }
 }
--- src/main/java/jimlind/announcecast/integration/action/SettingsAction.java
+++ src/main/java/jimlind/announcecast/integration/action/SettingsAction.java
@@ -1,6 +1,10 @@
 package jimlind.announcecast.integration.action;
 
 import com.google.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import jimlind.announcecast.discord.Manager;
+import jimlind.announcecast.integration.PopulatedTag;
 import jimlind.announcecast.integration.context.SettingsContext;
 import jimlind.announcecast.podcast.Client;
 import jimlind.announcecast.podcast.Podcast;
@@ -8,11 +12,14 @@ import jimlind.announcecast.storage.db.Feed;
 import jimlind.announcecast.storage.db.Patreon;
 import jimlind.announcecast.storage.db.PromotedFeed;
 import jimlind.announcecast.storage.db.Tag;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public class SettingsAction {
   @Inject Feed feed;
   @Inject Client client;
+  @Inject Manager manager;
   @Inject Patreon patreon;
   @Inject PromotedFeed promotedFeed;
   @Inject Tag tag;
@@ -25,8 +32,26 @@ public class SettingsAction {
     }
 
     String feedId = this.promotedFeed.getPromotedFeedIdByUserId(userId);
-    Podcast podcast = this.client.createPodcastFromFeedUrl(this.feed.getUrl(feedId), 1, 10);
+    Podcast promoted = this.client.createPodcastFromFeedUrl(this.feed.getUrl(feedId), 1, 10);
 
-    return new SettingsContext(podcast);
+    List<PopulatedTag> tagList = new ArrayList<>();
+    for (jimlind.announcecast.storage.model.Tag tag : this.tag.getTagsByUserId(userId)) {
+      String feedUrl = this.feed.getUrl(tag.getFeedId());
+      Podcast podcast = this.client.createPodcastFromFeedUrl(feedUrl, 1, 10);
+      Role role = this.manager.getRole(tag.getRoleId());
+      GuildChannel channel = this.manager.getChannel(tag.getChannelId());
+
+      if (podcast == null || channel == null || role == null) {
+        continue;
+      }
+
+      PopulatedTag populatedTag = new PopulatedTag();
+      populatedTag.setPodcastTitle(podcast.getTitle());
+      populatedTag.setRoleName(role.getName());
+      populatedTag.setChannelName(channel.getName());
+      tagList.add(populatedTag);
+    }
+
+    return new SettingsContext(promoted, tagList);
   }
 }
