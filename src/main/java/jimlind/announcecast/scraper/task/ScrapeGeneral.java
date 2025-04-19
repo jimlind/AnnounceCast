4b99e137d1b506d40d060c1c83b58691de788194
Jim Lind <spoon.vw@gmail.com>
2025-04-18 22:33:22 -0400

    debug: why?

--- src/main/java/jimlind/announcecast/scraper/task/ScrapeGeneral.java
+++ src/main/java/jimlind/announcecast/scraper/task/ScrapeGeneral.java
@@ -29,8 +29,10 @@ public class ScrapeGeneral extends TimerTask {
       return;
     }
     this.paginationIndex++;
+    System.out.println(this.paginationIndex);
 
     for (PostedFeed postedFeed : postedFeedList) {
+      System.out.println(postedFeed.getUrl());
       Podcast podcast = client.createPodcastFromFeedUrl(postedFeed.getUrl(), 1);
       if (podcast == null || podcast.getEpisodeList().isEmpty()) {
         continue;
