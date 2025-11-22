8d9c7b714d1846e86ecff6b850a435961edac8a9
Jim Lind <spoon.vw@gmail.com>
2025-11-22 15:15:18 -0500

    updates for dependency injection
    this took a little bit because this was part of the manager dependency injection cycle

--- src/main/java/jimlind/announcecast/discord/SlashCommand.java
+++ src/main/java/jimlind/announcecast/discord/SlashCommand.java
@@ -1,25 +1,64 @@
 package jimlind.announcecast.discord;
 
-import com.google.inject.Inject;
 import java.util.List;
-import jimlind.announcecast.discord.message.*;
-import jimlind.announcecast.integration.action.*;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import jimlind.announcecast.discord.message.FollowMessageList;
+import jimlind.announcecast.discord.message.FollowingMessageList;
+import jimlind.announcecast.discord.message.HelpMessageList;
+import jimlind.announcecast.discord.message.SearchMessageList;
+import jimlind.announcecast.discord.message.SettingsMessageList;
+import jimlind.announcecast.discord.message.UnfollowMessageList;
+import jimlind.announcecast.integration.action.FollowAction;
+import jimlind.announcecast.integration.action.FollowRssAction;
+import jimlind.announcecast.integration.action.FollowingAction;
+import jimlind.announcecast.integration.action.HelpAction;
+import jimlind.announcecast.integration.action.PrioritizeAction;
+import jimlind.announcecast.integration.action.SearchAction;
+import jimlind.announcecast.integration.action.SettingsAction;
+import jimlind.announcecast.integration.action.TagAction;
+import jimlind.announcecast.integration.action.UnfollowAction;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 @Slf4j
+@Singleton
 public class SlashCommand {
-  @Inject private FollowAction followAction;
-  @Inject private FollowingAction followingAction;
-  @Inject private FollowRssAction followRssAction;
-  @Inject private HelpAction helpAction;
-  @Inject private Manager manager;
-  @Inject private PrioritizeAction prioritizeAction;
-  @Inject private SearchAction searchAction;
-  @Inject private SettingsAction settingsAction;
-  @Inject private TagAction tagAction;
-  @Inject private UnfollowAction unfollowAction;
+  private final FollowAction followAction;
+  private final FollowingAction followingAction;
+  private final FollowRssAction followRssAction;
+  private final HelpAction helpAction;
+  private final Manager manager;
+  private final PrioritizeAction prioritizeAction;
+  private final SearchAction searchAction;
+  private final SettingsAction settingsAction;
+  private final TagAction tagAction;
+  private final UnfollowAction unfollowAction;
+
+  @Inject
+  public SlashCommand(
+      FollowAction followAction,
+      FollowingAction followingAction,
+      FollowRssAction followRssAction,
+      HelpAction helpAction,
+      Manager manager,
+      PrioritizeAction prioritizeAction,
+      SearchAction searchAction,
+      SettingsAction settingsAction,
+      TagAction tagAction,
+      UnfollowAction unfollowAction) {
+    this.followAction = followAction;
+    this.followingAction = followingAction;
+    this.followRssAction = followRssAction;
+    this.helpAction = helpAction;
+    this.manager = manager;
+    this.prioritizeAction = prioritizeAction;
+    this.searchAction = searchAction;
+    this.settingsAction = settingsAction;
+    this.tagAction = tagAction;
+    this.unfollowAction = unfollowAction;
+  }
 
   public boolean process(SlashCommandInteractionEvent event) {
     event.deferReply().queue();
