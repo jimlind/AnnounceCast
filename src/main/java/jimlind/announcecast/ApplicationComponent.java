870763068521cf363e586151f422e953c8565e46
Jim Lind <spoon.vw@gmail.com>
2025-11-22 13:56:44 -0500

    Initial setup of DaggerApplicationComponent

---
+++ src/main/java/jimlind/announcecast/ApplicationComponent.java
@@ -0,0 +1,26 @@
+package jimlind.announcecast;
+
+import dagger.Component;
+import javax.inject.Singleton;
+
+@Singleton
+@Component
+public interface ApplicationComponent {
+  // Listens to Discord events
+  jimlind.announcecast.discord.Listeners listeners();
+
+  // Manages the Discord connection
+  jimlind.announcecast.discord.Manager manager();
+
+  // Shuts the Discord connection down successfully
+  jimlind.announcecast.discord.ShutdownThread shutdownThread();
+
+  // Scrapes the podcast feeds to find most recent podcasts
+  jimlind.announcecast.scraper.Schedule scraperSchedule();
+
+  // Scrapes the promoted podcast feeds to find most recent podcasts
+  jimlind.announcecast.patreon.Schedule patreonSchedule();
+
+  // Special interface for the admin commands
+  jimlind.announcecast.administration.Action administrationAction();
+}
