792cb0f244b4d3f840f22e6431b6a3ef36e331ce
Jim Lind <spoon.vw@gmail.com>
2026-02-15 14:15:54 -0500

    Use the new format for the General Scraper

--- src/main/java/jimlind/announcecast/scraper/task/ScrapeGeneral.java
+++ src/main/java/jimlind/announcecast/scraper/task/ScrapeGeneral.java
@@ -1,28 +1,38 @@
 package jimlind.announcecast.scraper.task;
 
-import java.util.List;
+import jimlind.announcecast.core.taskScheduling.InfiniteFixedDelayTask;
+import jimlind.announcecast.storage.db.Joined;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import jimlind.announcecast.storage.db.Feed;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @Singleton
-public class ScrapeGeneral implements Runnable {
+public class ScrapeGeneral extends InfiniteFixedDelayTask {
+  private static final long INITIAL_DELAY_MILLIS = 0;
+  private static final long SUBSEQUENT_DELAY_MILLIS = 2000; // 5 seconds
+  private static final long TIMEOUT_MILLIS = 60000; // 1 minute
   private static final int PAGINATION_SIZE = 20;
-  private final Feed feed;
+
+  private final Joined joined;
   private final ScrapeSinglePodcastFactory scrapeFactory;
   private int paginationIndex = 0;
 
   @Inject
-  public ScrapeGeneral(Feed feed, ScrapeSinglePodcastFactory scrapeFactory) {
-    this.feed = feed;
+  public ScrapeGeneral(Joined joined, ScrapeSinglePodcastFactory scrapeFactory) {
+    super(INITIAL_DELAY_MILLIS, SUBSEQUENT_DELAY_MILLIS, TIMEOUT_MILLIS);
+    this.joined = joined;
     this.scrapeFactory = scrapeFactory;
   }
 
   @Override
-  public void run() {
-    List<String> urlList = feed.getFeedUrlPage(PAGINATION_SIZE, paginationIndex);
+  public void runTask() {
+    List<String> urlList = joined.getFollowedFeedUrlPage(PAGINATION_SIZE, paginationIndex);
+    Collections.shuffle(urlList);
+
     if (urlList.isEmpty()) {
       paginationIndex = 0;
       return;
