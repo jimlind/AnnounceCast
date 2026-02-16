9da7db1a8e914ab7fa09b51d483623132fc15a81
Jim Lind <spoon.vw@gmail.com>
2026-02-15 22:30:58 -0500

    Move ReadQueue to new Scheduler
    Also add logic that writes 2 day old episodes to success metrics to reduce thrash

--- src/main/java/jimlind/announcecast/scraper/task/ReadQueue.java
+++ src/main/java/jimlind/announcecast/scraper/task/ReadQueue.java
@@ -1,11 +1,6 @@
 package jimlind.announcecast.scraper.task;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import javax.inject.Inject;
-import javax.inject.Singleton;
+import jimlind.announcecast.core.taskScheduling.InfiniteFixedRateTask;
 import jimlind.announcecast.discord.Manager;
 import jimlind.announcecast.discord.message.EpisodeMessage;
 import jimlind.announcecast.podcast.Client;
@@ -18,8 +13,22 @@ import jimlind.announcecast.storage.db.Joined;
 import jimlind.announcecast.storage.model.PostedFeed;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Singleton
-public class ReadQueue implements Runnable {
+public class ReadQueue extends InfiniteFixedRateTask {
+  private static final long INITIAL_DELAY_MILLIS = 0;
+  private static final long PERIOD_MILLIS = 200;
+  private static final long TIMEOUT_MILLIS = 10000; // 10 seconds
+
   private final Channel channel;
   private final Client client;
   private final Helper helper;
@@ -30,6 +39,7 @@ public class ReadQueue implements Runnable {
   @Inject
   public ReadQueue(
       Channel channel, Client client, Helper helper, Joined joined, Manager manager, Queue queue) {
+    super(INITIAL_DELAY_MILLIS, PERIOD_MILLIS, TIMEOUT_MILLIS);
     this.channel = channel;
     this.client = client;
     this.helper = helper;
@@ -39,7 +49,7 @@ public class ReadQueue implements Runnable {
   }
 
   @Override
-  public void run() {
+  public void runTask() {
     // Trying to get from the queue will return null if empty so exit early
     String url = queue.getPodcast();
     if (url == null) {
@@ -53,8 +63,8 @@ public class ReadQueue implements Runnable {
 
     ArrayList<Episode> episodeList = new ArrayList<>();
     for (Episode episode : podcast.getEpisodeList()) {
-      // If episode is not processed add it to the list otherwise break the loop to avoid
-      // posting old episodes
+      // If episode is not processed (or not in the episode queue) add it to the list.
+      // Otherwise, break the loop to avoid posting old episodes.
       if (helper.episodeNotProcessed(episode, postedFeed)) {
         episodeList.add(episode);
       } else break;
@@ -64,10 +74,21 @@ public class ReadQueue implements Runnable {
       }
     }
 
+    // Loop over all episodes reversed (so oldest is first) and log old episodes as success
+    // This should help limit episode queue thrashing for podcasts that can't post
+    for (Episode episode : episodeList.reversed()) {
+      ZonedDateTime pubDateTime = jimlind.announcecast.Helper.stringToDate(episode.getPubDate());
+      Duration pubDateDifference = Duration.between(pubDateTime, ZonedDateTime.now());
+      if (pubDateDifference.getSeconds() > TimeUnit.HOURS.toSeconds(48)) {
+        helper.recordSuccessToDatabase(postedFeed.getId(), episode.getGuid());
+      }
+    }
+
+    // Loop over all episodes reversed (so oldest is first) attempting to post messages
     for (Episode episode : episodeList.reversed()) {
       queue.setEpisode(postedFeed.getId(), episode.getGuid());
       for (String channelId : channel.getChannelsByFeedId(postedFeed.getId())) {
-
+        // Process the episode for the channel
         List<String> role = this.joined.getTagsByFeedIdAndChannelId(postedFeed.getId(), channelId);
         Stream<String> stream = role.stream().map(input -> String.format("<@&%s>", input));
         String message = stream.collect(Collectors.joining(","));
