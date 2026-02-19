e3c6a8d3d17bb6d860a6396eea8f89432fe6fff9
Jim Lind <spoon.vw@gmail.com>
2026-02-18 21:18:45 -0500

    Translate final Scheduler Features

--- src/main/java/jimlind/announcecast/core/taskScheduling/Scheduler.java
+++ src/main/java/jimlind/announcecast/core/taskScheduling/Scheduler.java
@@ -1,11 +1,25 @@
 package jimlind.announcecast.core.taskScheduling;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+@Singleton
+@Slf4j
 public class Scheduler {
+  private final AtomicBoolean scheduledTasksStarted = new AtomicBoolean(false);
+  private final AtomicBoolean supervisorStarted = new AtomicBoolean(false);
   private final List<InfiniteTask> taskList = new ArrayList<>();
+  private final ScheduledExecutorService supervisor = Executors.newSingleThreadScheduledExecutor();
 
+  @Inject
   public Scheduler() {}
 
   public void addTask(InfiniteTask task) {
@@ -13,10 +27,30 @@ public class Scheduler {
   }
 
   public void startAll() {
+    if (!scheduledTasksStarted.compareAndSet(false, true)) {
+      log.error("Failure: Scheduler can only be started once.");
+      return;
+    }
+
     taskList.forEach(InfiniteTask::start);
+
+    if (supervisorStarted.compareAndSet(false, true)) {
+      supervisor.scheduleAtFixedRate(this::restartAnyExitedTasks, 1, 1, TimeUnit.SECONDS);
+    }
   }
 
   public void stopAll() {
     taskList.forEach(InfiniteTask::stop);
+    scheduledTasksStarted.set(false);
+  }
+
+  private void restartAnyExitedTasks() {
+    for (InfiniteTask task : taskList) {
+      if (!task.hasExited()) {
+        continue;
+      }
+      log.info("Restarting the exited task: {}", task.getClass().getSimpleName());
+      task.start();
+    }
   }
 }
