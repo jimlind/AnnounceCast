20e89e1d00f24a076afb31487d2fd99e09c2d13b
Jim Lind <spoon.vw@gmail.com>
2026-02-21 10:41:41 -0500

    Setup override methods in EmbedBuilder and add validation tests

--- src/main/java/jimlind/announcecast/discord/EmbedBuilder.java
+++ src/main/java/jimlind/announcecast/discord/EmbedBuilder.java
@@ -1,6 +1,9 @@
 package jimlind.announcecast.discord;
 
-import java.awt.*;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.awt.Color;
 
 public class EmbedBuilder extends net.dv8tion.jda.api.EmbedBuilder {
   private static final Color DEFAULT_COLOR = new Color(122, 184, 122);
@@ -8,5 +11,16 @@ public class EmbedBuilder extends net.dv8tion.jda.api.EmbedBuilder {
   public EmbedBuilder() {
     super();
     this.setColor(DEFAULT_COLOR);
+  }
+
+  @Override
+  public net.dv8tion.jda.api.@NonNull EmbedBuilder setTitle(@Nullable String title) {
+    return super.setTitle(title);
+  }
+
+  @Override
+  public net.dv8tion.jda.api.@NonNull EmbedBuilder setTitle(
+      @Nullable String title, @Nullable String url) {
+    return super.setTitle(title, url);
   }
 }
--- src/test/java/jimlind/announcecast/discord/EmbedBuilderTest.java
+++ src/test/java/jimlind/announcecast/discord/EmbedBuilderTest.java
@@ -1,11 +1,15 @@
 package jimlind.announcecast.discord;
 
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 public class EmbedBuilderTest {
   @Test
-  void createEmbedBuilder_withLongTitle_throwsException() {
+  void createEmbedBuilder_withLongTitle_shouldThrowException() {
     assertThrows(
         IllegalArgumentException.class,
         () -> {
@@ -13,5 +17,29 @@ public class EmbedBuilderTest {
           String longText = "a".repeat(300);
           embedBuilder.setTitle(longText);
         });
+  }
+
+  @Test
+  void createEmbedBuilder_withShortTitle_shouldReturnSameTitle() {
+    String uuid = UUID.randomUUID().toString();
+
+    EmbedBuilder embedBuilder = new EmbedBuilder();
+    embedBuilder.setTitle(uuid);
+    MessageEmbed messageEmbed = embedBuilder.build();
+    String title = messageEmbed.getTitle();
+
+    assertEquals(uuid, title);
+  }
+
+  @Test
+  void createEmbedBuilder_withShortTitleAndUrl_shouldReturnSameTitle() {
+    String uuid = UUID.randomUUID().toString();
+
+    EmbedBuilder embedBuilder = new EmbedBuilder();
+    embedBuilder.setTitle(uuid, "http://example.com");
+    MessageEmbed messageEmbed = embedBuilder.build();
+    String title = messageEmbed.getTitle();
+
+    assertEquals(uuid, title);
   }
 }
